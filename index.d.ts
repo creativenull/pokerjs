d0fb348974e058323d5074d36d35c56fb1fd9c40
Arnold Chand <creativenull@outlook.com>
2021-04-03 19:46:42 -0400

    feat: use standard instead of eslint, fix vulns

--- index.d.ts
+++ index.d.ts
@@ -1,17 +1,17 @@
 import { Deck, Card } from 'deckjs'
 
 export type Player = {
-  id: string;
-  hand: Card[];
+  id: string
+  hand: Card[]
 }
 
 export type PlayerResult = {
-  id: string;
-  handRank: number;
-  handRankKey: PokerHandKey;
-  tieBreakerCardRank: number;
-  name: PokerHandText;
-  tieBreakerTotalRank?: number;
+  id: string
+  handRank: number
+  handRankKey: PokerHandKey
+  tieBreakerCardRank: number
+  name: PokerHandText
+  tieBreakerTotalRank?: number
 }
 
 export type PokerHandText = ('Royal Flush' | 'Straight Flush' | 'Four-of-a-Kind' | 'Full House' | 'Flush' | 'Straight' |
@@ -21,32 +21,32 @@ export type PokerHandKey = ('ROYAL_FLUSH' | 'STRAIGHT_FLUSH' | 'FOUR_OF_A_KIND' 
                              'THREE_OF_A_KIND' | 'TWO_PAIR' | 'PAIR' | 'HIGH_CARD' | 'NONE')
 
 export type PokerHandRanking = {
-  ROYAL_FLUSH: number;
-  STRAIGHT_FLUSH: number;
-  FOUR_OF_A_KIND: number;
-  FULL_HOUSE: number;
-  FLUSH: number;
-  STRAIGHT: number;
-  THREE_OF_A_KIND: number;
-  TWO_PAIR: number;
-  PAIR: number;
-  HIGH_CARD: number;
-  NONE: number;
+  ROYAL_FLUSH: number
+  STRAIGHT_FLUSH: number
+  FOUR_OF_A_KIND: number
+  FULL_HOUSE: number
+  FLUSH: number
+  STRAIGHT: number
+  THREE_OF_A_KIND: number
+  TWO_PAIR: number
+  PAIR: number
+  HIGH_CARD: number
+  NONE: number
 }
 
 export type _StraightCheck = {
-  check: boolean;
-  straightRank: number;
+  check: boolean
+  straightRank: number
 }
 
 export type _FlushCheck = {
-  check: boolean;
-  flushRank: number;
+  check: boolean
+  flushRank: number
 }
 
 export type _PairsCheck = {
-  pairs: number[];
-  rank: number;
+  pairs: number[]
+  rank: number
 }
 
 declare class Poker extends Deck {
